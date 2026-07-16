263365bd29ed7a70ae4cc997f22af33a679aa4ec
1 <a1@MacBook-Pro-1.local>
2026-07-16 16:05:20 +0300

    Fetch fix

--- src/main/java/ru/ruslan/teamleadgame/config/WebConfig.java
+++ src/main/java/ru/ruslan/teamleadgame/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:5173",
+                        "https://tl-front-first.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
